f0588eb21c882bb66b03ef52e845a0cb3a4009d4
Cesar <cesar.gorriz@alu.uclm.es>
2024-04-05 20:34:53 +0200

    Algoritmo terminado falta mejoralo y hablarlo

--- src/main/java/org/example/CamionVoraz.java
+++ src/main/java/org/example/CamionVoraz.java
@@ -1,6 +1,39 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class CamionVoraz {
+
+    public void camion (ArrayList<Integer> mercancia ){
+        //ordenamos mercancia
+
+
+        if (mercancia.size()==0){
+            System.out.println("No hay mercancia");
+
+        }
+        int pesoCamion = 10000;
+
+        Collections.sort(mercancia);
+        int precio=0;
+        ArrayList<Integer> camion=new ArrayList<Integer>();
+        for (int m:mercancia.stream().toList( )) {
+            if (pesoCamion - m >= 0) {
+                camion.add(m);
+                precio=precio+40;
+                System.out.println("Introduciomos al camion un peso de "+ m+"kg y un precio de 40 Euros ");
+
+            }else {
+                return;
+            }
+        }
+        System.out.println("La factura es de "+precio + " han cabido "+ camion.size()+" productos.");
+
+    }
     public static void main(String[] args) {
         System.out.println("args = " + args);
     }
