51427a21b3717b83c0d84976ee81a54dcf856382
Cesar <cesar.gorriz@alu.uclm.es>
2024-04-07 19:36:55 +0200

    Pruebas de orden y mercancia del camion

--- src/main/java/org/example/CamionVoraz.java
+++ src/main/java/org/example/CamionVoraz.java
@@ -11,18 +11,22 @@ public class CamionVoraz {
     public ArrayList<Integer> pesoPaqueteCamion (ArrayList<Integer> mercancia ){
         //ordenamos mercancia
 
-
+        //comprobamos que la mercancia entrante no sea 0
         if (mercancia.size()==0){
             System.out.println("No hay mercancia");
             return new ArrayList<>();
         }
         int pesoCamion = 10000;
 
+        //ordemanomos la mercacia de menos kg a mas kg
         Collections.sort(mercancia);
 
+        //creamos el arry de camion y recorremos la mercancia con un for each
         ArrayList<Integer> camion=new ArrayList<Integer>();
         for (int m:mercancia.stream().toList( )) {
+            //calculamos que entre en el camion
             if (pesoCamion - m >= 0 ) {
+                //añadimos la carca y restamos el peso y lo mostramos por pantalla
                 camion.add(m);
                 pesoCamion=pesoCamion-m;
 
@@ -32,11 +36,13 @@ public class CamionVoraz {
                 return camion ;
             }
         }
+        //calculamos el precio del camion y motramos por pantalla
         int precio = precioCamion(camion);
         System.out.println("La factura es de "+precio + " han cabido "+ camion.size()+" productos.");
         return camion;
     }
 
+    //en el siguiente metodo calculamos el precio de la factura del camion
     public int precioCamion(ArrayList<Integer> camion) {
         return (camion.size())*40;
     }
