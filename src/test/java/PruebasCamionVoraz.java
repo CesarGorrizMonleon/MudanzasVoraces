b2d1fedc7282dd386cbb3259eade7075aa509973
Cesar <cesar.gorriz@alu.uclm.es>
2024-04-19 10:22:59 +0200

    Añadir dependencias del segundo trabajo

--- src/test/java/PruebasCamionVoraz.java
+++ src/test/java/PruebasCamionVoraz.java
@@ -1,9 +1,11 @@
 import org.example.CamionVoraz;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PruebasCamionVoraz {
 
