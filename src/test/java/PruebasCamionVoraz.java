8bb4d777afd88532129fd374285d94fd1d77e814
Miguel-Requena <miguel.requena@alu.uclm.es>
2024-04-20 13:22:57 +0200

    Programacion Dinamica

--- src/test/java/PruebasCamionVoraz.java
+++ src/test/java/PruebasCamionVoraz.java
@@ -1,9 +1,9 @@
 import org.example.CamionVoraz;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PruebasCamionVoraz {
 
