56003c0530f5139706c9b3f1d8fdfcc8b5a7ae7d
MiguelP-10 <129193535+MiguelP-10@users.noreply.github.com>
2024-04-26 10:12:56 +0200

    añadimos clase de pruebas a PD

---
+++ src/test/java/PruebasPD.java
@@ -0,0 +1,14 @@
+import org.example.CamionVoraz;
+import org.example.PD;
+
+public class PruebasPD {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        PD camion = new PD();
+    }
+
+    @test
+    public void f(){}
+
+}
