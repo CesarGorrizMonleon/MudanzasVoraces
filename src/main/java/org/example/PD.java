ed14be520100389368efc1523714df6590029fd2
Cesar <cesar.gorriz@alu.uclm.es>
2024-04-27 12:20:50 +0200

    Cambio de array a array list

--- src/main/java/org/example/PD.java
+++ src/main/java/org/example/PD.java
@@ -1,8 +1,8 @@
 package org.example;
 
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 import org.example.Mercancia;
 
 public class PD {
@@ -11,31 +11,36 @@ public class PD {
     private final static int PAGO = 6;
     public static void main(String[] args) {
         int c = CAPACIDAD;
-        Mercancia[] v = new Mercancia[5];
-        Mercancia m1 = new Mercancia(3,7);
-        v[0]=m1;
-        Mercancia m2 = new Mercancia(4,9);
-        v[1]=m2;
-        Mercancia m3 = new Mercancia(3,6);
-        v[2]= m3;
-        Mercancia m4 = new Mercancia(3,6);
-        v[3]=m4;
-        Mercancia m5 = new Mercancia(3,6);
-        v[4]=m5;
+        ArrayList<Mercancia> v = new ArrayList<>();
+        v.add(new Mercancia(3,7));
+        v.add(new Mercancia(4,9));
+        v.add(new Mercancia(3,6));
+        v.add(new Mercancia(3,6));
+        v.add(new Mercancia(3,6));
+
+
         pd(v,c);
 
     }
 
 
-    public static int[][] pd (Mercancia[] v, int C){
-        int f = v.length+1;
+    public static int[][] pd (ArrayList<Mercancia> tabla, int C){
+        
+
+       tabla.add(0, new Mercancia(0,0));
+
+
+        for (Mercancia d:tabla) {
+            System.out.println("d.peso = " + d.peso);
+        }
         int c = C +1;  //Columna para camión sin capacidad
-        int[][] T = new int[f][c];
+        int[][] T = new int[tabla.size()][c];
 
         //HACER TABLA
-        for(int i = 0; i < f; i++){
+        for(int i = 1; i < tabla.size(); i++){
             for(int j = 0; j < c; j++){
-                T[i][j]= Integer.max(T[i-1][j],v[i].beneficio + T[i-1][j-v[i].peso]);
+
+                T[i][j]= Integer.max(T[i-1][j],tabla.get(i).beneficio + T[i-1][j-tabla.get(i).peso]);
             }
         }
         // IMPRIMIR TABLA
@@ -44,8 +49,8 @@ public class PD {
             System.out.print(j + "\t");
         }
         System.out.println("\n-----------------------------------------------");
-        for (int i = 0; i < f; i++) {
-            System.out.print("Peso " + v[i] + "\t|\t");
+        for (int i = 0; i < tabla.size(); i++) {
+           System.out.print("Peso " + tabla.get(i) + "\t|\t");
             for (int j = 0; j < c; j++) {
                 System.out.print(T[i][j] + "\t");
             }
@@ -56,10 +61,10 @@ public class PD {
         int i, j, pesoT;
 
 
-        pesoT = T[f - 1][c - 1];
+        pesoT = T[tabla.size() - 1][c - 1];
         System.out.println("Capacidad maxima del camion: " + pesoT);
         System.out.println("Mercancias utilizadas: ");
-        i = f - 1;
+        i = tabla.size() - 1;
         j = c - 1;
         while (i > 0 && j > 0) {
             if (T[i][j] != T[i - 1][j]) {
