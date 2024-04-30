8b2c95abd0c8e42adf6dc2106c65ddeaac86536d
Cesar <cesar.gorriz@alu.uclm.es>
2024-04-30 17:52:35 +0200

    Finalizado la clase PD

--- src/main/java/org/example/PD.java
+++ src/main/java/org/example/PD.java
@@ -20,25 +20,32 @@ public class PD {
         tablaProgDinamica(v,c);
 
     }
-    public int calcularPrecioCamion (ArrayList<Mercancia> camion){
+    public static int calcularPrecioCamion (ArrayList<Mercancia> camion){
        if (camion.size()==0)return 0;
         int acumulado =0;
         for (Mercancia d:camion) {
             acumulado= acumulado+d.beneficio;
+            System.out.println("d.peso+\" \"+d.beneficio = " + d.peso+" "+d.beneficio);
         }
-        System.out.println("El precio  del  camión es" + camion+"€");
+        System.out.println("El precio  del  camión es" + acumulado+"€");
         return acumulado;
     }
 
-    public ArrayList<Mercancia> objetosSeleccionado(int[][] tabla){
+    public static ArrayList<Mercancia> objetosSeleccionado(int[][] tabla,ArrayList<Mercancia>mercancias){
+
+
         ArrayList<Mercancia> camion= new ArrayList<>();
         for (int i = CAPACIDAD; i > 0; i--) {
-            for (int j = tabla.length; j > 1 ; j--) {
-                if (tabla[i][j]!=tabla[i][j-1]){
+            for (int j = tabla.length-1; j > 0 ; j--) {
+                if (tabla[j][i]!=tabla[j-1][i]){
 
+                    camion.add(mercancias.get(j));
+                    i=i-mercancias.get(j).peso+1;
+                    break;
                 }
             }
         }
+        return camion;
     }
 
     public static int[][] tablaProgDinamica(ArrayList<Mercancia> tabla, int C){
@@ -68,15 +75,18 @@ public class PD {
         }
         System.out.println("\n-----------------------------------------------");
         for (int i = 0; i < tabla.size(); i++) {
-           System.out.print("Peso " + tabla.get(i) + "\t|\t");
+           System.out.print("Peso " + tabla.get(i).peso+ ','+tabla.get(i).beneficio+ "\t|\t");
             for (int j = 0; j < c; j++) {
                 System.out.print(T[i][j] + "\t");
             }
             System.out.println("\n");
         }
 
-        // MOSTRAR SOLUCION
 
+
+        // MOSTRAR SOLUCION
+        ArrayList<Mercancia> camion = objetosSeleccionado(T,tabla);
+        int precio = calcularPrecioCamion(camion);
         return T;
 
 //        int i, j, pesoT;
